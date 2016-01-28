8b50ea847d6917b64b5f6b50bf0af834b2c3ac3b
Deepak Agnihotri <deepak2007in@gmail.com>
2016-01-28 19:01:37 +0530

    Integrated the SharePoint Integration with Calculation Project

---
+++ Calculation/Sharepoint/Integration.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace SCI.CIProject.ProjectSaving
+{
+    public static class Integration
+    {
+        private static IDictionary<string, string> cipitem = new Dictionary<string, string>();
+        public static void SaveInformation(SharePointListItem item)
+        {
+            var properties = item.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                if (property.Name.ToUpper() != "INSTANCE")
+                {
+                    cipitem[property.Name] = property.GetValue(item).ToString();
+                }
+            }
+        }
+
+        public static SharePointListItem GetInformation()
+        {
+            var item = SharePointListItem.Instance;
+            var properties = item.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                if (property.Name.ToUpper() != "INSTANCE")
+                {
+                    var value = 0L;
+                    long.TryParse(cipitem[property.Name], out value);
+                    property.SetValue(item, value);
+                }
+            }
+            return item;
+        }
+    }
+}
