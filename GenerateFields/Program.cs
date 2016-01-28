3175227f9a2e4742c3a76c6d3bf5469947479200
Deepak Agnihotri <deepak2007in@gmail.com>
2016-01-28 15:25:22 +0530

    Basic code for adding and removing properties to sharepoint list

--- GenerateFields/Program.cs
+++ GenerateFields/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SharePoint;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,6 +7,8 @@ namespace GenerateFields
 {
     public class Program
     {
+        #region ENum
+
         enum Months
         {
             Jan,
@@ -44,12 +47,50 @@ namespace GenerateFields
             EstimatedSaving
         }
 
+        #endregion
+
         private static IList<string> properties = new List<string>();
+
+        private static string spSite;
+
+        private static string listName;
+
         public static void Main(string[] args)
         {
+            if (args.Length > 1)
+            {
+                spSite = args[0];
+                listName = args[1];
+            }
+            else
+            {
+                Console.Write("Enter value for SP Site");
+                spSite = Console.ReadLine();
+                Console.Write("Enter value for List Name");
+                listName = Console.ReadLine();
+            }
+
             PopulateMonthsForProjectTypes();
             PopulateCalculationForProjectTypes();
             PopulateOtherProperties();
+
+            Console.WriteLine("Choose operation to perform. \n\n 1 - Adding Properties \n 2 - Removing Properties \n 3 - Displaying Properties");
+            var operation = Console.ReadKey();
+            switch(operation.KeyChar)
+            {
+                case '1':
+                    AddListProperties();
+                    break;
+                case '2':
+                    RemoveListProperties();
+                    break;
+                case '3':
+                    DisplayProperties();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Selection.");
+                    break;
+            }
             Console.Read();
         }
 
@@ -76,6 +117,88 @@ namespace GenerateFields
         private static void PopulateOtherProperties()
         {
 
+        }
+
+        private static void AddListProperties()
+        {
+            using (var site = new SPSite(spSite))
+            {
+                site.AllowUnsafeUpdates = true;
+                using (var web = site.OpenWeb())
+                {
+                    web.AllowUnsafeUpdates = true;
+                    var list = web.Lists[listName];
+                    foreach (var propertyName in properties)
+                    {
+                        try
+                        {
+                            var field = (SPFieldNumber)list.Fields.CreateNewField(SPFieldType.Number.ToString(), propertyName);
+                            field.Required = false;
+                            field.DisplayFormat = SPNumberFormatTypes.NoDecimal;
+                            list.Fields.Add(field);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Unable to delete {0} - {1}", propertyName, ex.Message);
+                        }
+                    }
+                    try
+                    {
+                        list.Update();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Unable to update {0} - {1}", ex.Message, ex.StackTrace);
+                    }
+
+                    web.AllowUnsafeUpdates = false;
+                }
+                site.AllowUnsafeUpdates = false;
+            }
+        }
+
+        private static void RemoveListProperties()
+        {
+            using (var site = new SPSite(spSite))
+            {
+                site.AllowUnsafeUpdates = true;
+                using (var web = site.OpenWeb())
+                {
+                    web.AllowUnsafeUpdates = true;
+                    var list = web.Lists[listName];
+                    foreach (var propertyName in properties)
+                    {
+                        try
+                        {
+                            list.Fields.Delete(propertyName);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Unable to delete {0} - {1}", propertyName, ex.Message);
+                        }
+                    }
+                    try
+                    {
+                        list.Update();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Unable to update {0} - {1}", ex.Message, ex.StackTrace);
+                    }
+                    web.AllowUnsafeUpdates = false;
+                }
+                site.AllowUnsafeUpdates = false;
+            }
+        }
+
+        private static void DisplayProperties()
+        {
+            Console.WriteLine("Enter string literal");
+            var literal = Console.ReadLine();
+            foreach(var propertyName in properties)
+            {
+                Console.WriteLine(literal, propertyName);
+            }
         }
     }
 }
