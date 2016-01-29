6199a6d744b0bec5a37ad1868d3119ada36d1408
Deepak Agnihotri <deepak2007in@gmail.com>
2016-01-29 14:50:27 +0530

    Setting the text box values from Project Cost

--- SharePoint/CalculationControl.ascx.cs
+++ SharePoint/CalculationControl.ascx.cs
@@ -13,23 +13,87 @@ namespace SharePoint
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            cat1.Text = cat2.Text = cat3.Text = cat4.Text = cat5.Text = cat6.Text = cat7.Text = cat8.Text = cat9.Text = cat10.Text = cat11.Text = cat12.Text = "10000";
-            caa1.Text = caa2.Text = caa3.Text = caa4.Text = caa5.Text = caa6.Text = caa7.Text = caa8.Text = caa9.Text = caa10.Text = caa11.Text = caa12.Text = "5000";
-            crt1.Text = crt2.Text = crt3.Text = crt4.Text = crt5.Text = crt6.Text = crt7.Text = crt8.Text = crt9.Text = crt10.Text = crt11.Text = crt12.Text = "10000";
-            cra1.Text = cra2.Text = cra3.Text = cra4.Text = cra5.Text = cra6.Text = cra7.Text = cra8.Text = cra9.Text = cra10.Text = cra11.Text = cra12.Text = "4000";
-            rgt1.Text = rgt2.Text = rgt3.Text = rgt4.Text = rgt5.Text = rgt6.Text = rgt7.Text = rgt8.Text = rgt9.Text = rgt10.Text = rgt11.Text = rgt12.Text = "10000";
-            rga1.Text = rga2.Text = rga3.Text = rga4.Text = rga5.Text = rga6.Text = rga7.Text = rga8.Text = rga9.Text = rga10.Text = rga11.Text = rga12.Text = "6000";
-            cit1.Text = cit2.Text = cit3.Text = cit4.Text = cit5.Text = cit6.Text = cit7.Text = cit8.Text = cit9.Text = cit10.Text = cit11.Text = cit12.Text = "10000";
-            cia1.Text = cia2.Text = cia3.Text = cia4.Text = cia5.Text = cia6.Text = cia7.Text = cia8.Text = cia9.Text = cia10.Text = cia11.Text = cia12.Text = "7000";
+            //cat1.Text = cat2.Text = cat3.Text = cat4.Text = cat5.Text = cat6.Text = cat7.Text = cat8.Text = cat9.Text = cat10.Text = cat11.Text = cat12.Text = "10000";
+            //caa1.Text = caa2.Text = caa3.Text = caa4.Text = caa5.Text = caa6.Text = caa7.Text = caa8.Text = caa9.Text = caa10.Text = caa11.Text = caa12.Text = "5000";
+            //crt1.Text = crt2.Text = crt3.Text = crt4.Text = crt5.Text = crt6.Text = crt7.Text = crt8.Text = crt9.Text = crt10.Text = crt11.Text = crt12.Text = "10000";
+            //cra1.Text = cra2.Text = cra3.Text = cra4.Text = cra5.Text = cra6.Text = cra7.Text = cra8.Text = cra9.Text = cra10.Text = cra11.Text = cra12.Text = "4000";
+            //rgt1.Text = rgt2.Text = rgt3.Text = rgt4.Text = rgt5.Text = rgt6.Text = rgt7.Text = rgt8.Text = rgt9.Text = rgt10.Text = rgt11.Text = rgt12.Text = "10000";
+            //rga1.Text = rga2.Text = rga3.Text = rga4.Text = rga5.Text = rga6.Text = rga7.Text = rga8.Text = rga9.Text = rga10.Text = rga11.Text = rga12.Text = "6000";
+            //cit1.Text = cit2.Text = cit3.Text = cit4.Text = cit5.Text = cit6.Text = cit7.Text = cit8.Text = cit9.Text = cit10.Text = cit11.Text = cit12.Text = "10000";
+            //cia1.Text = cia2.Text = cia3.Text = cia4.Text = cia5.Text = cia6.Text = cia7.Text = cia8.Text = cia9.Text = cia10.Text = cia11.Text = cia12.Text = "7000";
         }
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
-            var processingMonths = this.ProcessImplementationDate();
-            this.ProcessTotalCost(processingMonths);
+            var processingMonths = this.ProcessTimeline();
+            this.ProcessCost(processingMonths);
         }
 
-        private int[] ProcessImplementationDate()
+        private TextBox[] TargetTextBoxesForCA
+        {
+            get
+            {
+                return new[] { cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12 };
+            }
+        }
+
+        private TextBox[] ActualTextBoxesForCA
+        {
+            get
+            {
+                return new[] { caa1, caa2, caa3, caa4, caa5, caa6, caa7, caa8, caa9, caa10, caa11, caa12 };
+            }
+        }
+
+        private TextBox[] TargetTextBoxesForCR
+        {
+            get
+            {
+                return new[] { crt1, crt2, crt3, crt4, crt5, crt6, crt7, crt8, crt9, crt10, crt11, crt12 };
+            }
+        }
+
+        private TextBox[] ActualTextBoxesForCR
+        {
+            get
+            {
+                return new[] { cra1, cra2, cra3, cra4, cra5, cra6, cra7, cra8, cra9, cra10, cra11, cra12 };
+            }
+        }
+
+        private TextBox[] TargetTextBoxesForRG
+        {
+            get
+            {
+                return new[] { rgt1, rgt2, rgt3, rgt4, rgt5, rgt6, rgt7, rgt8, rgt9, rgt10, rgt11, rgt12 };
+            }
+        }
+
+        private TextBox[] ActualTextBoxesForRG
+        {
+            get
+            {
+                return new[] { rga1, rga2, rga3, rga4, rga5, rga6, rga7, rga8, rga9, rga10, rga11, rga12 };
+            }
+        }
+
+        private TextBox[] TargetTextBoxesForCI
+        {
+            get
+            {
+                return new[] { cit1, cit2, cit3, cit4, cit5, cit6, cit7, cit8, cit9, cit10, cit11, cit12 };
+            }
+        }
+
+        private TextBox[] ActualTextBoxesForCI
+        {
+            get
+            {
+                return new[] { cia1, cia2, cia3, cia4, cia5, cia6, cia7, cia8, cia9, cia10, cia11, cia12 };
+            }
+        }
+
+        private int[] ProcessTimeline()
         {
             var implementationDateStr = txtImplementationDate.Text;
             var implementationDate = DateTime.ParseExact(implementationDateStr, "MM/dd/yyyy", CultureInfo.InvariantCulture);
@@ -59,7 +123,7 @@ namespace SharePoint
             return new int[0];
         }
 
-        private void ProcessTotalCost(int[] months)
+        private ProjectCost ProcessCost(int[] months)
         {
             var capEx = long.Parse(txtcapExp.Text);
             var implementationCost = long.Parse(txtimplcost.Text);
@@ -71,23 +135,18 @@ namespace SharePoint
 
             lbltotal.Text = projectCost.TotalCost.ToString();
 
-            var targets = new[] { cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12 };
-            var actuals = new[] { caa1, caa2, caa3, caa4, caa5, caa6, caa7, caa8, caa9, caa10, caa11, caa12 };
-            this.ProcessCheckBoxes(projectCost, chkCostAvoidance, ProjectType.CostAvoidance, targets, actuals, months);
-
-            targets = new[] { crt1, crt2, crt3, crt4, crt5, crt6, crt7, crt8, crt9, crt10, crt11, crt12 };
-            actuals = new[] { cra1, cra2, cra3, cra4, cra5, cra6, cra7, cra8, cra9, cra10, cra11, cra12 };
-            this.ProcessCheckBoxes(projectCost, chkCostReduction, ProjectType.CostReduction, targets, actuals, months);
-
-            targets = new[] { rgt1, rgt2, rgt3, rgt4, rgt5, rgt6, rgt7, rgt8, rgt9, rgt10, rgt11, rgt12 };
-            actuals = new[] { rga1, rga2, rga3, rga4, rga5, rga6, rga7, rga8, rga9, rga10, rga11, rga12 };
-            this.ProcessCheckBoxes(projectCost, chkRevenueGrowth, ProjectType.RevenueGrowth, targets, actuals, months);
-
-            targets = new[] { cit1, cit2, cit3, cit4, cit5, cit6, cit7, cit8, cit9, cit10, cit11, cit12 };
-            actuals = new[] { cia1, cia2, cia3, cia4, cia5, cia6, cia7, cia8, cia9, cia10, cia11, cia12 };
-            this.ProcessCheckBoxes(projectCost, chkCapacityIncrease, ProjectType.CapacityIncrease, targets, actuals, months);
+            this.ProcessCheckBoxes(projectCost, chkCostAvoidance, ProjectType.CostAvoidance, this.TargetTextBoxesForCA, this.ActualTextBoxesForCA, months);
+            this.ProcessCheckBoxes(projectCost, chkCostReduction, ProjectType.CostReduction, this.TargetTextBoxesForCR, this.ActualTextBoxesForCR, months);
+            this.ProcessCheckBoxes(projectCost, chkRevenueGrowth, ProjectType.RevenueGrowth, this.TargetTextBoxesForRG, this.ActualTextBoxesForRG, months);
+            this.ProcessCheckBoxes(projectCost, chkCapacityIncrease, ProjectType.CapacityIncrease, this.TargetTextBoxesForCI, this.ActualTextBoxesForCI, months);
 
             var sharepointListItem = SharePointListItem.Convert(projectCost, int.Parse(lblmnth1.Text));
+            this.ProcessTotal(sharepointListItem, projectCost, months);
+            return projectCost;
+        }
+
+        private void ProcessTotal(SharePointListItem sharepointListItem, ProjectCost projectCost, int[] months)
+        {
             twelvtxtcavalue.Text = sharepointListItem.SavedOver12Months_CA.ToString();
             twelvtxtcrvalue.Text = sharepointListItem.SavedOver12Months_CR.ToString();
             twelvtxtrgvalue.Text = sharepointListItem.SavedOver12Months_RG.ToString();
@@ -106,10 +165,10 @@ namespace SharePoint
             esttxtci.Text = sharepointListItem.EstimatedSaving_CI.ToString();
             estlbltotalval.Text = sharepointListItem.EstimatedSaving_Total.ToString();
 
-            targets = new[] { target1total, target2total, target3total, target4total, target5total, target6total, target7total, target8total, target9total, target10total, target11total, target12total };
+            var targets = new[] { target1total, target2total, target3total, target4total, target5total, target6total, target7total, target8total, target9total, target10total, target11total, target12total };
             this.ProcessFooter(targets, months, projectCost.GetTargetTotal);
 
-            actuals = new[] { actual1total, actual2total, actual3total, actual4total, actual5total, actual6total, actual7total, actual8total, actual9total, actual10total, actual11total, actual12total };
+            var actuals = new[] { actual1total, actual2total, actual3total, actual4total, actual5total, actual6total, actual7total, actual8total, actual9total, actual10total, actual11total, actual12total };
             this.ProcessFooter(actuals, months, projectCost.GetActualTotal);
         }
 
@@ -134,6 +193,21 @@ namespace SharePoint
             {
                 var parseResult = long.TryParse(actuals[index].Text, out cost);
                 projectCost.SetCostEntry(projectType, CostType.Actual, months[index], checkBox.Checked && parseResult ? cost : 0);
+            }
+        }
+
+        private void ProcessCheckBoxes(ProjectCost projectCost, ProjectType projectType, TextBox[] targets, TextBox[] actuals, int[] months)
+        {
+            for (var index = 0; index < months.Length; index++)
+            {
+                var cost = projectCost.GetCostEntry(projectType, CostType.Target, months[index]);
+                targets[index].Text = cost.ToString();
+            }
+
+            for (var index = 0; index < months.Length; index++)
+            {
+                var cost = projectCost.GetCostEntry(projectType, CostType.Actual, months[index]);
+                actuals[index].Text = cost.ToString();
             }
         }
     }
