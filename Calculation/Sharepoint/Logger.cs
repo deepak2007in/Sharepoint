089bd3140eacd1d48753dd44981499065fb85f95
Deepak Agnihotri <deepak2007in@gmail.com>
2016-02-24 14:59:57 +0530

    Final Commit

---
+++ Calculation/Sharepoint/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCI.CIProject.ProjectSaving
+{
+    public static class Logger
+    {
+        const string fileName = "D://abc.txt";
+        public static void Log(string message)
+        {
+            File.AppendAllLines(fileName, new[] {message});
+        }
+
+        public static void LogStackTrace()
+        {
+            var stackTrace = new StackTrace();
+            var frames = stackTrace.GetFrames();
+            var collection = string.Empty;
+            foreach (var frame in frames)
+            {
+                collection += frame.ToString() + "|";
+            }
+            File.AppendAllLines(fileName, collection.Split('|'));
+        }
+    }
+}
